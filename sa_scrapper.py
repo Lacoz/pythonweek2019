93cf6d5f1fa56bd32db4f1156ecf6f7dd73f955b
Ladislav Zigo <zigo@profesia.sk>
2019-06-29 12:37:37 +0200

    reformat

--- sa_scrapper.py
+++ sa_scrapper.py
@@ -1,17 +1,16 @@
 from requests_html import HTMLSession
 import argparse
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument("--source")
 parser.add_argument("--destination")
 parser.add_argument("--departure_date")
 args = parser.parse_args()
 
-
 session = HTMLSession()
 r = session.get('https://www.studentagency.cz/data/wc/ybus-form/destinations-sk.json')
 site_data = r.json()
+
 
 def get_city_id(city_name, site_data):
     for dest in site_data['destinations']:
@@ -20,23 +19,20 @@ def get_city_id(city_name, site_data):
                 return city['id']
 
 
-source_id = get_city_id(args.source,site_data)
-destination_id = get_city_id(args.destination,site_data)
+source_id = get_city_id(args.source, site_data)
+destination_id = get_city_id(args.destination, site_data)
 departure_date = args.departure_date
-
 
 s_url = "https://brn-ybus-pubapi.sa.cz/restapi/routes/search/simple"
 
-
-r = session.get(s_url, params = {'locale':'sk',
-                        'departureDate':departure_date,
-                        'fromLocationId':source_id,
-                        'toLocationId':destination_id,
-                        'fromLocationType':'CITY',
-                        'toLocationType':'CITY',
-                        'tarrifs':'REGULAR'})
+r = session.get(s_url, params={'locale': 'sk',
+                               'departureDate': departure_date,
+                               'fromLocationId': source_id,
+                               'toLocationId': destination_id,
+                               'fromLocationType': 'CITY',
+                               'toLocationType': 'CITY',
+                               'tarrifs': 'REGULAR'})
 routes_data = r.json()
-
 
 output = []
 for route in routes_data['routes']:
